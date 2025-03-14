a6ef33d4b00fb5b1bc4f21066bcd80dcbc9d020a
JSH <skyjs135@gmail.com>
2025-03-14 16:32:32 +0900

    feat: 프로젝트 설정 변경 - Spring Batch, Thymeleaf, MockK 라이브러리 추가
    - build.gradle
      - Spring Batch 추가 (배치 작업 지원)
      - Thymeleaf 추가 (템플릿 엔진)
      - MockK 추가 (테스트용 목킹 라이브러리)

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,12 +32,17 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	testImplementation("org.springframework.batch:spring-batch-test")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("io.mockk:mockk:1.13.17")
+
 }
 
 kotlin {
