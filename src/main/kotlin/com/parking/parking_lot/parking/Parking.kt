a7dc3fad94cc82e5e04942662f783eedc32e135a
JSH <skyjs135@gmail.com>
2025-03-23 13:09:10 +0900

    refactor: Parking 엔티티 ID를 nullable로 변경하여 자동 할당되도록 수정
    - Parking
      - ID를 nullable(val id: Long? = null)로 변경하여, 엔티티 생성 시 자동 할당되도록 수정
    
    - ParkingItemReader
      - ID nullable 변경에 따른 DTO → 엔티티 변환 로직 수정

--- src/main/kotlin/com/parking/parking_lot/parking/Parking.kt
+++ src/main/kotlin/com/parking/parking_lot/parking/Parking.kt
@@ -2,33 +2,37 @@ package com.parking.parking_lot.parking
 
 import com.parking.parking_lot.operationHours.OperatingHours
 import jakarta.persistence.*
+import org.hibernate.annotations.BatchSize
 
 @Entity
 @Table(name = "parking")
 data class Parking(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,  // Primary Constructor에서 id를 nullable로 유지
 
-        @Column(nullable = false)
-        val name: String,//주차장 이름
+    @Column(nullable = false)
+    val name: String,//주차장 이름
 
-        val address: String? = "", //도로명 주소
+    val address: String? = "", //도로명 주소
 
-        val latitude: Double,//위도
+    val latitude: Double,//위도
 
-        val longitude: Double,//경도
-
-
-        val totalSpots: Int,//주차장 최대수용
+    val longitude: Double,//경도
 
 
-        val feePerHour: Int,//시간당요금
-
-        @Enumerated(EnumType.STRING)
-        val ownership: Ownership?= Ownership.PUBLIC,//공영/민영
+    val totalSpots: Int,//주차장 최대수용
 
 
-        @OneToMany(mappedBy = "parking", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-        val operatingHours: List<OperatingHours> = listOf()
+    val feePerHour: Int,//시간당요금
+
+    @Enumerated(EnumType.STRING)
+    val ownership: Ownership?= Ownership.PUBLIC,//공영/민영
+
+
+    @OneToMany(mappedBy = "parking", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    @BatchSize(size = 1000)
+    val operatingHours: List<OperatingHours> = listOf()
+
+
 )
